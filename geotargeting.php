f1c1cc1a232808d8879036a40af935c15cf8a943
timersys <timersys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-03 22:35:56 +0000

    tagging v1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/geotargeting/trunk@1081886 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- geotargeting.php
+++ geotargeting.php
@@ -16,7 +16,7 @@
  * Plugin Name:       GeoTargeting
  * Plugin URI:        http://wp.timersys.com/geotargeting/
  * Description:       Geo Targeting for WordPress will let you country-target your content based on users IP's and Geocountry Ip database
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Timersys
  * Author URI:        http://wp.timersys.com/geotargeting/
  * License:           GPL-2.0+
@@ -29,7 +29,7 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
-
+@session_start();
 define( 'GEOT_PLUGIN_FILE' , __FILE__);
 define( 'GEOT_PLUGIN_DIR' , plugin_dir_path(__FILE__) );
 define( 'GEOT_PLUGIN_URL' , plugin_dir_url(__FILE__) );
